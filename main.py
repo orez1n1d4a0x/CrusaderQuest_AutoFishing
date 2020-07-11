647a05ca4bd122d0ce21a7b95516a9dc27b6c08e
orez1n1d4a0x <orez1n1d4a0x@gmail.com>
2020-07-11 20:17:37 +0800

    fix logic

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import utility as _u
 import threading
-import time
+from time import sleep
 def drop_bobber():
 	e_imgfound=threading.Event()
 	e_stop=threading.Event()
-	x=_u.get_imgctr('wheel.png')
-	_u.pos_click(*x)
+	searchArea=_u.getArea()
+	pos_wheel=_u.get_imgctr('wheel.png')
+	_u.pos_click(*pos_wheel)
 	sleep(0.5)
-	th_bobber=threading.Thread(target=_u.img_loop_detect, args=('to_drop_bobber.png', e_imgfound, e_stop, _u.getArea()))
+	th_bobber=threading.Thread(target=_u.img_loop_detect, args=('to_drop_bobber.png', e_imgfound, e_stop, searchArea))
 	th_bobber.start()
 	e_imgfound.wait(timeout=10)
 	if e_imgfound.is_set():
-		_u.pos_click(*x)
+		_u.pos_click(*pos_wheel)
 		print('click')
 		e_stop.set()
 		th_bobber.join()
