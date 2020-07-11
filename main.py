8fba499be6a9ba1170e34bdc9ca19fa60a0576db
orez1n1d4a0x <orez1n1d4a0x@gmail.com>
2020-07-11 18:44:20 +0800

    get wheel first

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ import time
 def drop_bobber():
 	e_imgfound=threading.Event()
 	e_stop=threading.Event()
+	x=_u.get_imgctr('wheel.png')
 	th_bobber=threading.Thread(target=_u.img_loop_detect, args=('do_not_drop.png', e_imgfound, e_stop, _u.getArea()))
 	th_bobber.start()
 	e_imgfound.wait(timeout=10)
 	if e_imgfound.is_set():
-		_u.pos_click(*_u.get_imgctr('wheel.png'))
+		_u.pos_click(*x)
 		e_stop.set()
 		th_bobber.join()
 		return True
